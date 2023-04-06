9e26b734506bd9a19bfd014e2d9a94bbff36ab74
verozaiter <veronica.zaiter@gmail.com>
2023-04-06 10:30:13 -0300

    Add Ejs Leetcode C#

---
+++ EjsLeetCode/EjsLeetCode/RunningSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjsLeetCode
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                nums[i] = sum;
+            }
+            return nums;
+        }
+    }
+}
