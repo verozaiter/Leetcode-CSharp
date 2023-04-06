9c7e76c918f6c4578ea3fd7a9d991fe6d22e1201
verozaiter <veronica.zaiter@gmail.com>
2023-04-06 13:58:12 -0300

    Add new exercise

---
+++ EjsLeetCode/EjsLeetCode/NumberOfSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjsLeetCode
+{
+    /* https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/ */
+    public class Solution1
+    {
+        public int NumberOfSteps(int num)
+        {
+            int numSteps = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+                else
+                {
+                    num -= 1;
+                }
+                numSteps++;
+            }
+
+            return numSteps;
+        }
+    }
+}
